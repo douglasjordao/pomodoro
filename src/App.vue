8c58058101338dcc0a48c7d21a2596b7cfad5fc0
Douglas Jordão <douglasjordao@gmail.com>
2025-08-16 00:57:18 -0300

    Refactory appearance
    - Add a border on explanation card
    - Add a empty template to data table in session card

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 import Clock from './components/Clock.vue';
 import SessionList from './components/SessionList.vue';
 import PToast from 'primevue/toast';
-import PPanel from 'primevue/panel';
 
 import logo from './assets/logo.png';
+import Explanation from '@/components/Explanation.vue';
 </script>
 
 <template>
@@ -38,36 +38,7 @@ import logo from './assets/logo.png';
         <SessionList />
       </div>
 
-      <PPanel class="pm-what-is-pomodoro">
-        <template #default>
-          <div class="flex align-items-center">
-            <img :src="logo" alt="Pomodoro" class="pm-logo" />
-            <h2>What is the Pomodoro Technique?</h2>
-          </div>
-
-          <p class="px-4 text-lg">
-            The Pomodoro Technique is a productivity method that alternates
-            periods of intense focus with short breaks. Traditionally, you work
-            for 25 minutes with full concentration, followed by a 5-minute
-            break. After 4 cycles, you take a longer break of 15 to 30 minutes.
-            This practice helps maintain focus, prevent mental fatigue, and
-            improve time management.
-          </p>
-
-          <div class="flex align-items-center">
-            <img :src="logo" alt="Pomodoro" class="pm-logo" />
-            <h2>How does it work?</h2>
-          </div>
-
-          <ul class="px-4 text-lg">
-            <li>Just hit Start and you're ready to go.</li>
-            <li>
-              If you want to customize your timers, click the clock you'd like
-              to edit.
-            </li>
-          </ul>
-        </template>
-      </PPanel>
+      <Explanation />
     </div>
   </div>
 
@@ -87,18 +58,4 @@ body {
 }
 </style>
 
-<style lang="scss" scoped>
-.pm-logo {
-  width: 72px;
-  height: 72px;
-}
-
-.pm-what-is-pomodoro {
-  max-width: 465px;
-  height: 570px;
-
-  @media screen and (min-width: 992px) {
-    width: 800px;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
--- src/components/SessionList.vue
+++ src/components/SessionList.vue
@@ -6,6 +6,8 @@ import PTag from 'primevue/tag';
 import { useTimerStore } from '@/store/modules/timerStore';
 import CardContainer from './CardContainer.vue';
 
+import logo from '@/assets/logo.png';
+
 const timerStore = useTimerStore();
 
 function getTagSeverity(type: string) {
@@ -31,21 +33,53 @@ function getTagSeverity(type: string) {
         scrollHeight="250px"
         class="pm-sessions__table"
       >
-        <PColumn field="time" header="Time">
+        <template #empty>
+          <div class="flex align-items-center justify-content-center">
+            <img
+              :src="logo"
+              alt="Pomodoro"
+              class="pm-logo"
+              style="width: 40px; height: 40px"
+            />
+            <h2 class="pm-sessions__title ml-4">No sessions yet</h2>
+          </div>
+        </template>
+
+        <PColumn field="time">
+          <template #header>
+            <span class="pm-sessions__header">Time</span>
+          </template>
           <template #body="{ data }">
-            <PTag :value="data.time" :severity="getTagSeverity(data.type)" />
+            <PTag
+              :value="data.time"
+              :severity="getTagSeverity(data.type)"
+              class="pm-sessions__tag"
+            />
           </template>
         </PColumn>
-        <PColumn field="label" header="Type">
+
+        <PColumn field="label">
+          <template #header>
+            <span class="pm-sessions__header">Type</span>
+          </template>
           <template #body="{ data }">
-            <PTag :value="data.label" :severity="getTagSeverity(data.type)" />
+            <PTag
+              :value="data.label"
+              :severity="getTagSeverity(data.type)"
+              class="pm-sessions__tag"
+            />
           </template>
         </PColumn>
-        <PColumn field="duration" header="Duration">
+
+        <PColumn field="duration">
+          <template #header>
+            <span class="pm-sessions__header">Duration</span>
+          </template>
           <template #body="{ data }">
             <PTag
               :value="data.duration"
               :severity="getTagSeverity(data.type)"
+              class="pm-sessions__tag"
             />
           </template>
         </PColumn>
@@ -56,12 +90,33 @@ function getTagSeverity(type: string) {
 
 <style lang="scss" scoped>
 .pm-sessions {
+  height: 375px;
+  width: 298px;
+
   @media screen and (min-width: 576px) {
+    width: 465px;
+
     &__option {
       &-text,
       &-icon {
         font-size: 0.5em;
       }
+    }
+  }
+
+  &__header {
+    font-size: 0.8em;
+
+    @media screen and (min-width: 576px) {
+      font-size: 1em;
+    }
+  }
+
+  &__tag {
+    font-size: 0.65em;
+
+    @media screen and (min-width: 576px) {
+      font-size: 1em;
     }
   }
 }
